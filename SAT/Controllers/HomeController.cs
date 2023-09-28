d2b62a6fab484d3433602d3e57ffa5a958c055c1
Blake Harrison <Blakemharrison@outlook.com>
2023-09-28 13:26:37 -0500

    Changed site color dynamic / logo

--- SAT/Controllers/HomeController.cs
+++ SAT/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace SAT.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
